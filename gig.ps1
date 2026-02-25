e85fd06a775c20dc1f654c3dbdd0faa317c58b06
Tigran TIKSN Torosyan <personal@tiksn.com>
2026-02-25 16:15:42 -0600

    feat: Initialize Fable project for Fossa.Bridge, including a Validation service and DTOs.

---
+++ gig.ps1
@@ -0,0 +1,18 @@
+#For PowerShell v3
+Function gig {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string[]]$list
+    )
+    $params = ($list | ForEach-Object { [uri]::EscapeDataString($_) }) -join ','
+    Invoke-WebRequest -Uri "https://www.toptal.com/developers/gitignore/api/$params"
+    | Select-Object -ExpandProperty content | Out-File -FilePath $(Join-Path -Path $pwd -ChildPath '.gitignore') -Encoding ascii
+}
+
+gig -list visualstudio, visualstudiocode, rider, fsharp, powershell, macos, intellij+all
+
+Add-Content -Path .\.gitignore -Value '# Repository Specific' -Encoding ascii
+Add-Content -Path .\.gitignore -Value '.trash' -Encoding ascii
+Add-Content -Path .\.gitignore -Value '.idea/' -Encoding ascii
+Add-Content -Path .\.gitignore -Value 'dist/' -Encoding ascii
+Add-Content -Path .\.gitignore -Value 'fable_output/' -Encoding ascii
